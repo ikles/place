898cf19d1210415111d54307b71d7bebd4531375
Igor <membship@ya.ru>
2021-05-21 23:20:56 +0700

    first commit

---
+++ app/result/js/common.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function( $ ) {
+
+
+  $('.filter-open').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('enable');
+    $('.catalog-col-l').toggleClass('show');
+    $('.catalog-col-r').toggleClass('catalog-col-r-full');
+  });
+
+
+  $('.sproduct-quantity-controls').each(function () {
+    let inputt = $(this).find('.sproduct-quantity-input');
+    let minuss = $(this).find('.sproduct-quantity-minus');
+    let pluss = $(this).find('.sproduct-quantity-plus');
+
+    pluss.click(function (e) {
+      e.preventDefault();
+      input_val = +inputt.attr('value') + 1;
+      inputt.attr('value', input_val);
+    });
+
+    minuss.click(function (e) {
+      e.preventDefault();
+      if (+inputt.attr('value') > 1) {
+        input_val = +inputt.attr('value') - 1;
+        inputt.attr('value', input_val);
+      }    
+    });
+  });
+
+}); //ready
+
